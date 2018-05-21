8746ed7735dd0203fb31667faec97ab1e7762010
jerauld <25723428+jerauld@users.noreply.github.com>
2018-05-21 08:59:48 -0700

    added logging fs.appendFile for data results, ran through a series of test to ensure log.txt logging is functional

--- liri.js
+++ liri.js
@@ -28,19 +28,26 @@ function spotifySearch(song){
         //Error Status
         if (err) {
           return console.log('Error occurred: ' + err);
+          fs.appendFile('log.txt', "Spotify Error occured: " + err + "\n");
         }
+
         //On success
         for (var i = 0; i < data.tracks.items.length; i++) {
           console.log("Artist: " + data.tracks.items[i].artists[0].name);
           console.log("Track name: " + data.tracks.items[i].name);
-          if (data.tracks.items[i].preview_url === null) {
+          fs.appendFile('log.txt', "Artist: " + data.tracks.items[i].artists[0].name + "\n");
+          fs.appendFile('log.txt', "Track name: " + data.tracks.items[i].name + "\n");
+          if (data.tracks.items[i].preview_url === null) { //Checks if song preview exists
             console.log("Song preview: Preview for this track is not available.")
+            fs.appendFile('log.txt', "Song preview: Preview for this track is not available.\n");
           } else {
             console.log("Song preview: " + data.tracks.items[i].preview_url);
+            fs.appendFile('log.txt', "Song preview: " + data.tracks.items[i].preview_url + "\n");
           }
           console.log("Album: " + data.tracks.items[i].album.name);
+          fs.appendFile('log.txt', "Album: " + data.tracks.items[i].album.name + "\n");
         }
-        console.log("\n");
+        console.log("\n"); //Styling
     });
 }
 
@@ -51,71 +58,53 @@ function twitterStatuses() {
         if (!error) {
             for (var i = 0; i < tweets.length; i++){
             console.log(i+1 + ". " + tweets[i].created_at.replace(/ \+0000/g, "") + " - " + tweets[i].text); //Get rid of the " +0000 on .creat_at"
+            fs.appendFile('log.txt', tweets[i].created_at.replace(/ \+0000/g, "") + " - " + tweets[i].text + "\n");
             }
-            console.log("\n");
+            console.log("\n"); //Styling
         }
     });
 }
 
 function omdbResults(movieName){
-
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    // This line is just to help us debug against the actual URL.
-    // console.log(queryUrl);
-
     request(queryUrl, function(error, response, body) {
-
-    // If the request is successful
         if (!error && response.statusCode === 200) {
-
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            // console.log(JSON.parse(body));
             var movieResponse = JSON.parse(body);
             if (movieResponse.Title === undefined) {
                 console.log("Movie not found in the OMDb database. Try another search.")
+                fs.appendFile('log.txt', "Movie not found in the OMDb database.\n");
             } else {
                 console.log("Movie Title: " + movieResponse.Title);
                 console.log("Release Date: " + movieResponse.Year);
                 console.log("IMDB Rating: " + movieResponse.Ratings[0].Value);
-                if (movieResponse.Ratings[1] === undefined) {
+                fs.appendFile('log.txt', "Movie Title: " + movieResponse.Title + "\n");
+                fs.appendFile('log.txt', "Release Date: " + movieResponse.Year + "\n");
+                fs.appendFile('log.txt', "IMDB Rating: " + movieResponse.Ratings[0].Value + "\n");
+                if (movieResponse.Ratings[1] === undefined) { //Checks if Rotten Tomatoes Rating exists
                     console.log("Rotten Tomatoes Rating: Not available for this movie title.")
+                    fs.appendFile('log.txt', "Rotten Tomatoes Rating: Not available for this movie title.\n");
                 } else {
                     console.log("Rotten Tomatoes Rating: " + movieResponse.Ratings[1].Value);
+                    fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + movieResponse.Ratings[1].Value + "\n");
                 }
                 console.log("Country: " + movieResponse.Country);
                 console.log("Plot: " + movieResponse.Plot);
                 console.log("Actors: " + movieResponse.Actors);
-                
+                fs.appendFile('log.txt', "Country: " + movieResponse.Country + "\n");
+                fs.appendFile('log.txt', "Plot: " + movieResponse.Plot + "\n");
+                fs.appendFile('log.txt', "Actors: " + movieResponse.Actors + "\n");
             }
-            console.log("\n");
+            console.log("\n"); //Styling
         }
     })
 }
 
 //RESULTS HEADERS
 
-function tweetHeader() {
+function resultsHeader(heading) {
     console.log("\n");
     console.log("===================================================================================================================");
-    console.log("# My Latest 20 Tweets");
-    console.log("===================================================================================================================");
-    console.log("\n");
-}
-
-function spotifyHeader() {
-    console.log("\n");
-    console.log("===================================================================================================================");
-    console.log("# Spotify Song Info");
-    console.log("===================================================================================================================");
-    console.log("\n");
-}
-
-function movieHeader() {
-    console.log("\n");
-    console.log("===================================================================================================================");
-    console.log("# OMDb Movie Info");
+    console.log("# " + heading);
     console.log("===================================================================================================================");
     console.log("\n");
 }
@@ -126,6 +115,7 @@ function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function(err, data) {
         if(err) {
             return console.log(error);
+            fs.appendFile('log.txt', "File-System Read File Error occured: " + error + "\n");
         }
         var dataArr = data.split(", ");
         var randomOperator = dataArr[0];
@@ -133,15 +123,18 @@ function doWhatItSays() {
     
         switch (randomOperator) {
             case "my-tweets":
-                tweetHeader();
+                resultsHeader("My 20 Latest Tweets");
+                fs.appendFile('log.txt', "random.txt file command: " + randomOperator + ". file argument: " + randomString + "\n");
                 twitterStatuses();
                 break;
             case "spotify-this-song":
-                spotifyHeader();
+                resultsHeader("Spotify Song Info");
+                fs.appendFile('log.txt', "random.txt file command: " + randomOperator + ". random.txt file argument: " + randomString + "\n");
                 spotifySearch(randomString);
                 break;
             case "movie-this":
-                movieHeader();
+                resultsHeader("OMDb Movie Info");
+                fs.appendFile('log.txt', "random.txt file command: " + randomOperator + ". random.txt file argument: " + randomString + "\n");
                 omdbResults(randomString);
                 break;
         }
@@ -156,27 +149,27 @@ switch (operator) {
             liriError("liri.js " + operator + userInputString.replace(/\+/g, " "));
             fs.appendFile('log.txt', "User Input Error: liri.js " + operator + userInputString.replace(/\+/g, " ") + " is an invalid command or argument.\n");
         } else {
-            fs.appendFile('log.txt', "User command: my-tweets \n");
-            tweetHeader();
+            resultsHeader("My 20 Latest Tweets");
             twitterStatuses();
+            fs.appendFile('log.txt', "User command: my-tweets \n");
         }
         break;
     case "spotify-this-song":
-        spotifyHeader();
+        resultsHeader("Spotify Song Info");
         if ((process.argv[3] === undefined) && (userInput.length <= 3)) {
-            fs.appendFile('log.txt', "User Input Error: User command: " + operator + " User arguments: null.\n");
             console.log("Error: User is missing an argument after the 'spotify-this-song' command.\n");
+            fs.appendFile('log.txt', "User Input Error: User command: " + operator + " User arguments: null.\n");
         } else {
-            fs.appendFile('log.txt', "User command: " + operator + ". User arguments: " + userInputString.replace(/\+/g, " ") + "\n");
             spotifySearch(userInputString.substring(1)); //Improves search results
+            fs.appendFile('log.txt', "User command: " + operator + ". User arguments: " + userInputString.replace(/\+/g, " ") + "\n");
         }
         break;
     case "movie-this":
-        movieHeader();
+        resultsHeader("OMDb Movie Info");
         if ((process.argv[3] === undefined) && (userInput.length <= 3)) {
-            fs.appendFile('log.txt', "User Input Error: User command: " + operator + " User arguments: null. Results for \"Mr. Nobody\" automatically shown.\n");
             console.log("Error Handling: No arugment input by user. Results for \"Mr. Nobody\" automatically shown.\n");
             omdbResults("Mr.+Nobody");
+            fs.appendFile('log.txt', "User Input Error: User command: " + operator + " User arguments: null. Results for \"Mr. Nobody\" automatically shown.\n");
         } else {
             fs.appendFile('log.txt', "User command: " + operator + ". User arguments: " + userInputString.replace(/\+/g, " ") + "\n");
             omdbResults(userInputString.substring(1)); //Improve search results
@@ -188,6 +181,7 @@ switch (operator) {
             fs.appendFile('log.txt', "User Input Error: liri.js " + operator + userInputString.replace(/\+/g, " ") + " is an invalid command or argument. \n");
         } else {
             doWhatItSays();
+            fs.appendFile('log.txt', "User command: do-what-it-says \n");
         }
         break;
     default: 
